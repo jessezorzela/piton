ca00bd842ced2a817677ee8b6fc39277301117c8
Jessé zorzela <69878221+jessezorzela@users.noreply.github.com>
2022-11-13 19:30:49 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -6,10 +6,41 @@ class conta_corrente:
         self.agencia = ''
         self.conta = ''
         self.senha = ''
-        self.saldo = 0.00
+        self.saldo = 0
         self.autenticado = False
 
+    def depositar(self, valor):	
+        self.saldo += valor
+        self.extrato(valor)
 
+    def sacar(self, valor):	
+        if valor <= self.saldo:
+            self.saldo -= valor
+            self.extrato(-valor)
+        else:
+            print('Saldo insuficiente.')
+    
+    def extrato(self, ultimo = 0):
+        print()
+        print('-' * 40)
+        print(f'                EXTRATO           ')
+        print('-' * 40)
+        print('LANÇAMENTOS')
+        if ultimo > 0:
+            print(f' DEPÓSITO ATM |     CRÉDITO {ultimo:.2F} ')
+        print('              |                |')
+        print('              |                |')
+        print('-' * 40)
+        print(f'                        SALDO: R$ {self.saldo:.2F}')
+        print('-' * 40)
+        print('             FIM DO EXTRATO				  ')
+        print('-' * 40)
+        aperte = input('Aperte ENTER para retornar ao MENU\nou (X e ENTER) para sair: ')
+        if aperte == 'X' or aperte == 'x': 
+            exit()
+        else:
+            self.executa()
+            
         executa = True
         while executa:
             print("Informe o que voce deseja fazer \n \
@@ -30,64 +61,10 @@ class conta_corrente:
                 self.agencia = input("informe sua agência> ")
                 self.conta = input("informe o numero da conta> ")
                 self.senha = input("informe sua senha> ")
-                pass
             if comando == 3:
-                self.agencia = input("informe o valor do deposito> ")
-                pass
+                valor = float(input('Digite o valor a ser depositado: '))
+                self.depositar(valor)
             if comando == 4:
-                self.agencia = input("informe o valor do deposito> ")
-                pass
+                self.extrato(-valor)
             if comando == 5:
-                print("Seu saldo é de" self.saldo)
-                pass
-            
-    def get_saldo(self):
-        return self.saldo
-
-    def deposita(self,valor: float):
-        if valor > 0:
-            self.saldo += valor
-        else:
-            print('Valor para deposito tem que ser maior que zero')
-
-
-    def get_agencia(self):
-        return self.agencia
-
-    def ag(self,valor: float):
-        return self.agencia
-
-    def get_conta(self):
-        return self.conta
-
-    def con(self,valor: float):
-        return self.conta
-
-    def get_senha(self):
-        return self.senha
-
-    def sen(self,valor: float):
-        return self.senha
-
-
-    def get_saldo(self):
-        return self.saldo
-
-    def deposita(self,valor: float):
-        if valor > 0:
-            self.saldo += valor
-        else:
-            print('Valor para deposito tem que ser maior que zero')
-        
-
-    def saca(self,valor: float):
-        if valor <= self.saldo and valor > 0:
-            self.saldo = self.saldo - valor 
-        elif valor == 0:
-            print('O valor a ser sacado tem que ser maior que zero!')  
-        else:
-            print('O valor a ser sacado não pode ser menor ou igual o saldo')        
-
-conta = conta_corrente()
-
-
+                self.extrato()
